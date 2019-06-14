cc2384c9b46fdd811f8be080aeabd63e85b0cee0
Jonathan <jonathanthomaz96@gmail.com>
2019-06-13 21:02:34 -0300

    teste class crud

--- src/testes/NewMain.java
+++ src/testes/NewMain.java
@@ -5,7 +5,9 @@
  */
 package testes;
 
+import java.lang.reflect.InvocationTargetException;
 import model.Equipe;
+import utils.CRUD;
 
 /**
  *
@@ -15,16 +17,20 @@ public class NewMain {
 
     /**
      * @param args the command line arguments
+     * @throws java.lang.ClassNotFoundException
+     * @throws java.lang.NoSuchMethodException
+     * @throws java.lang.IllegalAccessException
+     * @throws java.lang.reflect.InvocationTargetException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
         // TODO code application logic here
         Equipe equipe = new Equipe();
-        String nome ="Vasco";
-        equipe.setNomeEquipe(nome);
-        equipe.save();
+        String nome ="Cruzeiro";
+        equipe.setNome(nome);
+        System.out.println(equipe.getNome());
         
-        
-        
+        CRUD.CRUD("Equipe","save",equipe);
+     
     }
     
 }
